911421f037cdc98f32887e5061e4c14861339f36
Ad_zira <aladdintirta@gmail.com>
2022-02-11 09:41:46 +0700

    Updated the .gitignore file

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import cors from 'cors';
 import express from 'express';
-
 const app = express();
+const morgan = require('morgan');
+
 const PORT = process.env.PORT || 3003;
 
-import routes from './routes'
+const routes = require('./routes')
+const logger = require('./logs')
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+app.use(morgan('combined', { stream: logger.stream }))
 app.use('/', routes)
 
 app.listen(PORT, () => {
